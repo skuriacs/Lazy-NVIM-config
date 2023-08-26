189347255a2cb078e045f2832c0ee81033894eea
Samuel Kuriakose <skuriacs@gmail.com>
2023-08-26 05:57:42 -0400

    LuaLine Changes

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,23 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = function()
-		return {}
+		local function lsp_progress()
+			local messages = vim.lsp.util.get_progress_messages()
+			if #messages == 0 then
+				return ""
+			end
+			local status = {}
+			for _, msg in pairs(messages) do
+				table.insert(status, (msg.percentage or 0) .. "%% " .. (msg.title or ""))
+			end
+			local spinners = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+			local ms = vim.loop.hrtime() / 1000000
+			local frame = math.floor(ms / 120) % #spinners
+			local return_val = table.concat(status, " | ") .. " " .. spinners[frame + 1]
+			return return_val
+		end
+		return {
+			sections = { lualine_c = { lsp_progress }, lualine_x = { "buffers" } },
+		}
 	end,
 }
